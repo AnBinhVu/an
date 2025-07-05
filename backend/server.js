21fa1019f943016b43abc355f04b9b3749c700a8
AnBinhVu <127610567+AnBinhVu@users.noreply.github.com>
2025-07-05 14:47:30 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -65,7 +65,7 @@ app.get('*', (req, res) => {
 });
 
 // Server listen
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () =>
   console.log(`🚀 Server running at http://localhost:${PORT}`)
 );
