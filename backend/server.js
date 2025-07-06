678ff65ff1236962f437e2526750941a2d2a02b9
AnBinhVu <127610567+AnBinhVu@users.noreply.github.com>
2025-07-06 08:41:30 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -57,7 +57,7 @@ app.use('/api/plans', planRoutes);
 app.use('/api/payment', vnpayRoutes);
 
 // Serve React frontend (build từ frontend/build)
-const frontendPath = path.join(__dirname, 'backend/frontend/build');
+const frontendPath = path.join(__dirname, '/frontend');
 app.use(express.static(frontendPath));
 
 app.get('*', (req, res) => {
